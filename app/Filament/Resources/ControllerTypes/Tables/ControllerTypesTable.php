c7a2e4808525b765a1e437418f708622ebcdd40e
warih_suryono <warih_suryono@yahoo.com>
2026-07-16 14:01:34 +0700

    CRUD Controller Type

--- app/Filament/Resources/ControllerTypes/Tables/ControllerTypesTable.php
+++ app/Filament/Resources/ControllerTypes/Tables/ControllerTypesTable.php
@@ -2,13 +2,8 @@
 
 namespace App\Filament\Resources\ControllerTypes\Tables;
 
-use Filament\Actions\BulkActionGroup;
-use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
-use Filament\Actions\ForceDeleteBulkAction;
-use Filament\Actions\RestoreBulkAction;
 use Filament\Tables\Columns\TextColumn;
-use Filament\Tables\Filters\TrashedFilter;
 use Filament\Tables\Table;
 
 class ControllerTypesTable
